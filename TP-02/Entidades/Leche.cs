d2ddc2978d94408d81acdfc6579d269568102209
facundo876 <40859125+facundo876@users.noreply.github.com>
2019-10-08 16:36:52 -0300

    TP Nº2 Facundo Arce 2C

---
+++ TP-02/Entidades/Leche.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades_2018
+{
+    public class Leche : Producto
+    {
+        
+        private ETipo tipo;
+
+        public enum ETipo { Entera, Descremada }
+
+        #region Constructores
+
+        public Leche(EMarca marca, string patente, ConsoleColor color, ETipo tipo)
+            : base(patente, marca, color)
+        {
+            this.tipo = tipo;
+        }
+        
+        /// <summary>
+        /// Por defecto, TIPO será ENTERA
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="patente"></param>
+        /// <param name="color"></param>
+        public Leche(EMarca marca, string patente, ConsoleColor color)
+            :this(marca, patente, color, Leche.ETipo.Entera)
+        {
+            
+        }
+
+        #endregion
+
+        #region Propiedades
+        /// <summary>
+        /// Las leches tienen 20 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 20;
+            }
+        }
+
+        #endregion
+
+        #region Metodos
+        /// <summary>
+        ///     Publica todos los datos del Producto Dulce.
+        /// </summary>
+        /// <returns></returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("LECHE");
+            sb.AppendLine("TIPO : " + this.tipo);
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("CALORIAS : {0}", this.CantidadCalorias);
+            
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        #endregion
+    }
+}
